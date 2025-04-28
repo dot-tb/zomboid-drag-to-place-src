50b13a1931862b878f9b28374849bc28a002ad19
Tibère Borel <tibere.borel@outlook.com>
2025-04-28 16:34:49 +0200

    Moved inventory tetris patch to code runner ui

--- client/DelranDragToPlace/Patches/DelranDragToPlace_InventoryTetrisPatch.lua
+++ client/DelranDragToPlace/Patches/DelranDragToPlace_InventoryTetrisPatch.lua
@@ -11,11 +11,6 @@ local ItemGridContainerUI = require("InventoryTetris/UI/Container/ItemGridContai
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function ISInventoryPane:onMouseMove(dx, dy)
-    --[[
-        if true then return end;
-        --]]
-    --dprint("onMouseMove")
-    --ORIGINAL_ISInventoryPane_onMouseMove(self);
     if DragToPlace.placingItem and DragToPlace.startedFrom == self and not DragToPlace.hidden then
         DragToPlace:HideCursor();
     end
@@ -67,9 +62,10 @@ function DragItemRenderer:render()
     ORIGINAL_DragItemRenderer_render(self);
 end
 
-local EquipmentUITypes = { "EquipmentUI", "EquipmentSlot", "EquipmentSuperSlot", "WeaponSlot", "HotbarSlot" }
+--[[ local EquipmentUITypes = { "EquipmentUI", "EquipmentSlot", "EquipmentSuperSlot", "WeaponSlot", "HotbarSlot" }
 
 local function CancelOnMouseUp(UIElement)
+    if not DragToPlace.placingItem then return end;
     -- Event should always come from an ISInventoryPane, except for EquipmentUI
     if UIElement.Type ~= "ISInventoryPane" then
         -- If we got a mouse up on a EquipmentUI ui, stop no matter what
@@ -81,75 +77,10 @@ local function CancelOnMouseUp(UIElement)
         end
         -- If the cursor is hidden, it means that we are over UI,
         --   then we need to cancel the drag
-    elseif DragToPlace.placingItem and DragToPlace:IsHidden() then
+    elseif DragToPlace:IsHidden() then
         DragToPlace:Stop();
     end
 end
-
--- I have no idea wtf I am doingg
-
----@diagnostic disable-next-line: duplicate-set-field
-function ISInventoryPane:onMouseUpOutside(x, y)
-    --ORIGINAL_ISInventoryPane_onMouseUpOutside(self, x, y);
-    --DelranDragToPlace.WaitBeforeShowCursorTimer:Reset();
-    if not DragToPlace.hidden and self == DragToPlace.startedFrom then
-        DragToPlace:PlaceItem();
-    else
-        ORIGINAL_ISInventoryPane_onMouseUpOutside(self, x, y);
-        CancelOnMouseUp(self);
-    end
-end
-
-ORIGINAL_ItemGridContainerUI_onMouseMoveOutside = ORIGINAL_ItemGridContainerUI_onMouseMoveOutside or
-    ItemGridContainerUI.onMouseMoveOutside;
-function ItemGridContainerUI:onMouseMoveOutside(x, y)
-    ---@diagnostic disable-next-line: duplicate-set-field
-    --[[
-        if true then return end;
-        --]]
-    --ORIGINAL_ISInventoryPane_onMouseMoveOutside(self);
-    --dprint("onMouseMoveOutside")
-    ORIGINAL_ItemGridContainerUI_onMouseMoveOutside(self, x, y);
-    --dprint(DragAndDrop:isDragging(), " ", DragToPlace.placingItem)
-    --[[     if DragToPlace.placingItem and DragToPlace.startedFrom == self then
-        local isMouseOverUI = DelranUtils.IsMouseOverUI();
-        if DragToPlace.hidden and not isMouseOverUI then
-            DragToPlace:StartShowCursorTimer();
-        elseif not DragToPlace.hidden and isMouseOverUI then
-            DragToPlace:HideCursor();
-        end
-    elseif DragAndDrop:isDragging() and not DragToPlace.placingItem then
-        local vanillaStack = DragAndDrop.getDraggedStacks();
-        if not vanillaStack then return end;
-
-        local draggedItems = nil;
-        if vanillaStack.items then
-            -- Handle drag and drop from EquipmentUI
-            draggedItems = vanillaStack.items;
-        elseif vanillaStack[1] then
-            draggedItems = { vanillaStack[1].items[1] };
-            -- Handle drag and drop from ItemGridUI
-        end
-        if draggedItems then
-            DragToPlace:Start(getPlayer(), draggedItems, self);
-        end
-    end ]]
-    --CancelOnMouseUp(self);
-end
-
---[[
-ORIGINAL_ItemGridContainerUI_onMouseUpOutside = ORIGINAL_ItemGridContainerUI_onMouseUpOutside or
-    ItemGridContainerUI.onMouseUpOutside;
-function ItemGridContainerUI:onMouseUpOutside(x, y)
-    ORIGINAL_ItemGridContainerUI_onMouseUpOutside(self, x, y);
-end
-
-ORIGINAL_ItemGridUI_onMouseMoveOutside = ORIGINAL_ItemGridUI_onMouseMoveOutside or ItemGridUI.onMouseMoveOutside;
-function ItemGridUI:onMouseMoveOutside(x, y, gridStack)
-    --CancelOnMouseUp(self);
-    ORIGINAL_ItemGridUI_onMouseMoveOutside(self, x, y, gridStack);
-end
- ]]
 
 -- Equipment UI patch
 -- Equipment UI only has globals so...
@@ -184,3 +115,4 @@ function HotbarSlot:onMouseUp(x, y)
     CancelOnMouseUp(self);
     ORIGINAL_HotbarSlot_onMouseUp(self, x, y);
 end
+ ]]
